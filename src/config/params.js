69ea117fba34cd006c88c7db30a9b6f40894523c
Andrew Seldenrust <andrew.seldenrust@gmail.com>
2018-11-15 13:07:57 -0600

    Scrubbing config

--- src/config/params.js
+++ src/config/params.js
@@ -1,8 +1,2 @@
 export const firebase = {
-    apiKey: "AIzaSyDVlvf1B_cWRKtS3c4xPUZ86X7VnRqLO3w",
-    authDomain: "dropcorn-01.firebaseapp.com",
-    databaseURL: "https://dropcorn-01.firebaseio.com",
-    projectId: "dropcorn-01",
-    storageBucket: "dropcorn-01.appspot.com",
-    messagingSenderId: "258368875615"
 }
